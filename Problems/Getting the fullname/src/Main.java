8752cf9e7eb047f6791a0d38cc3154855c9913d9
vetrivel-muthusamy <vetrivel.muthusamy@hotmail.com>
2020-12-12 20:58:22 +0530

    Stage 1/4: Card anatomy
    Problems Added for Getters and Setters section

---
+++ Problems/Getting the fullname/src/Main.java
@@ -0,0 +1,32 @@
+class User {
+    private String firstName;
+    private String lastName;
+
+    public User() {
+        this.firstName = "";
+        this.lastName = "";
+    }
+
+    public void setFirstName(String firstName) {
+        // write your code here
+        if (firstName != null) {
+            this.firstName = firstName;
+        }
+    }
+
+    public void setLastName(String lastName) {
+        // write your code here
+        if (lastName != null) {
+            this.lastName = lastName;
+        }
+    }
+
+    public String getFullName() {
+        if (firstName.isEmpty() && lastName.isEmpty()) {
+            return "Unknown";
+        }
+
+        String fullName = firstName + " " + lastName;
+        return fullName.trim();
+    }
+}
